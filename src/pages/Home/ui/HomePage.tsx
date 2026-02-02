23bae493e035ba58419d4dd79ce4b7a0658a8d41
Eduard Muliar <edikmulyar228@gmail.com>
2026-02-02 15:55:02 +0200

    feat: Implement internationalization (i18n) using i18next and react-i18next with initial English and Ukrainian locales and basic page routing

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -2,13 +2,21 @@ import { Link } from "react-router";
 
 import styles from "./HomePage.module.scss";
 import { useTheme } from "@/shared/config";
+import { useTranslation } from "react-i18next";
 
 const HomePage = () => {
   const { toggleTheme } = useTheme();
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = () => {
+    i18n.changeLanguage(i18n.language === "en" ? "ua" : "en");
+  };
+
   return (
     <>
-      <h1 className={styles.title}>Home</h1>
+      <h1 className={styles.title}>{t("hello")}</h1>
       <button onClick={toggleTheme}>Toggle theme</button>
+      <button onClick={changeLanguage}>language</button>
       <Link to={"/login"}>Login page</Link>
     </>
   );
