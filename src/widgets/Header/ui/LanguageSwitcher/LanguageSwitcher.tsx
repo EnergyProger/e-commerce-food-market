c80762e824733d83e8c3e154f350f26e7d0cbb2b
Eduard Muliar <edikmulyar228@gmail.com>
2026-02-03 17:28:24 +0200

    feat: Implement Header component with theme and language switchers

---
+++ src/widgets/Header/ui/LanguageSwitcher/LanguageSwitcher.tsx
@@ -0,0 +1,27 @@
+import { AppIcon, Button } from "@/shared/ui";
+import { useTranslation } from "react-i18next";
+import {
+  ENGLISH_LANGUAGE,
+  languageIconList,
+  UKRAINIAN_LANGUAGE,
+  type SupportedLngsType,
+} from "@/shared/config";
+
+export const LanguageSwitcher = () => {
+  const { i18n } = useTranslation();
+
+  const currentLanguage = i18n.language as SupportedLngsType;
+
+  const toggleChangeLanguage = () =>
+    i18n.changeLanguage(
+      i18n.language === ENGLISH_LANGUAGE
+        ? UKRAINIAN_LANGUAGE
+        : ENGLISH_LANGUAGE,
+    );
+
+  return (
+    <Button theme="ghost" onClick={toggleChangeLanguage}>
+      <AppIcon Icon={languageIconList[currentLanguage]} />
+    </Button>
+  );
+};
