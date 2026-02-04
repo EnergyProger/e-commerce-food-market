b9daee46e1949f9b45213c39fc84fb1c2503faa6
Eduard Muliar <edikmulyar228@gmail.com>
2026-02-04 19:35:14 +0200

    feat: Set up Vitest and React Testing Library for unit testing, including configuration and initial tests for `classNames`  function and `Button` component

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -10,5 +12,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/shared/config/test/setupTests.ts",
   },
 });
